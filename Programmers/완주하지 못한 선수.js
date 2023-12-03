7268b8887bc468f6b729c5bdfdeedaee161e078e
nasilKiM <nasikun@naver.com>
2023-12-03 20:30:59 +0900

    :memo: js 문제풀이 day02

--- Programmers/완주하지 못한 선수.js
+++ Programmers/완주하지 못한 선수.js
@@ -44,3 +44,39 @@ function solution(participant, completion) {
   // 모든 완주자와 일치한 경우 == 마지막 참여자가 완주하지 못한 선수
   return participant[participant.length - 1];
 }
+
+// =========================================================
+
+//참고할만한 다른사람 문제풀이 --1
+var solution = (_, $) =>
+  _.find(
+    (_) => !$[_]--,
+    $.map((_) => ($[_] = ($[_] | 0) + 1))
+  );
+
+/*문제풀이해설
+
+- solution 함수는 두 개의 매개변수 _와 $를 받습니다.
+- $ 배열을 map 함수를 사용하여 객체로 변환합니다. 이 객체는 각 이름의 등장 횟수를 나타내는데, 초기값은 0입니다.
+- find 함수를 사용하여 참가자 배열 _에서 등장 횟수를 감소시키면서 찾습니다. 만약 등장 횟수가 0이 되면 해당 선수는 완주하지 못한 선수입니다.
+  */
+
+// 참고할만한 다른사람 문제풀이 --2
+
+function solution(participant, completion) {
+  const map = new Map();
+
+  for (let i = 0; i < participant.length; i++) {
+    let a = participant[i],
+      b = completion[i];
+
+    map.set(a, (map.get(a) || 0) + 1);
+    map.set(b, (map.get(b) || 0) - 1);
+  }
+
+  for (let [k, v] of map) {
+    if (v > 0) return k;
+  }
+
+  return "nothing";
+}
